2faa71aa5c47ae6e024a7783c66869d2249825c1
Chris Ward <zwiebelspaetzle@gmail.com>
2021-01-21 16:11:05 -0700

    first commit with files

---
+++ src/SecretManager.php
@@ -0,0 +1,59 @@
+<?php
+namespace Zwiebelspaetzle\SecretManager;
+
+class SecretManager
+{
+    private $secrets = [];
+
+    public function getSecret(string $key)
+    {
+        return $this->secrets[$key]->retrieve();
+    }
+
+    public function newStaticSecret(string $key, $value)
+    {
+        $this->secrets[$key] = new StaticSecret($value);
+    }
+
+    public function newRenewableSecret(string $key, Callable $fetcher, Callable $checker = null)
+    {
+        $this->secrets[$key] = new RenewableSecret($fetcher, $checker);
+    }
+}
+
+class StaticSecret
+{
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    public function retrieve()
+    {
+        return $this->value;
+    }
+}
+
+class RenewableSecret
+{
+    private $value = null;
+    protected $fetcher;  // Callable
+    protected $checker;  // Callable
+
+    public function __construct(callable $fetcher, callable $checker)
+    {
+        $this->fetcher = $fetcher;
+        $this->checker = $checker;
+    }
+
+    public function retrieve()
+    {
+        if ($this->value === null || \Closure::fromCallable($this->checker)($this->value) == false) {
+            $this->value = \Closure::fromCallable($this->fetcher)();
+        } else {
+        }
+        return $this->value;
+    }
+}
---
+++ tests/unit/SecretManagerUnitTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use \Zwiebelspaetzle\SecretManager\SecretManager;
+
+class SecretManagerUnitTest extends TestCase
+{
+    public function setUp(): void
+    {
+        $this->secretManager = new SecretManager();
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->secretManager);
+    }
+
+    public function testSimpleSecret()
+    {
+        $this->secretManager->newStaticSecret('username', 'bob@example.com');
+        $this->assertEquals('bob@example.com', $this->secretManager->getSecret('username'));
+    }
+
+    public function testRenewableSecret()
+    {
+        $this->secretManager->newRenewableSecret(
+            'time-test',
+            [$this, 'fetcher'],
+            [$this, 'checker']
+        );
+
+        $begin = time();
+        $this->assertEquals($begin, $this->secretManager->getSecret('time-test'));
+
+        sleep(1); // wait enough to change time, but not expire secret
+        $this->assertNotEquals($begin, time()); // time should be different now...
+        $this->assertNotEquals(time(), $this->secretManager->getSecret('time-test')); // ... but the secret the same
+
+        sleep(3);  // wait for secret to expire. make sure we get a new one
+        $this->assertEquals(time(), $this->secretManager->getSecret('time-test'));
+    }
+
+    public function fetcher()
+    {
+        return time();
+    }
+
+    public function checker($secret) {
+        $valid = (time() - $secret < 4);
+        return $valid;
+    }
+}
